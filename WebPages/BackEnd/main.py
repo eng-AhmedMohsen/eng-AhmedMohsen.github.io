b05995affe539d8ef7c3e3703a4f59469d806856
eng-AhmedMohsen <94079413+eng-AhmedMohsen@users.noreply.github.com>
2024-01-27 21:07:30 +0200

    Update main.py

--- WebPages/BackEnd/main.py
+++ WebPages/BackEnd/main.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     username = request.form.get('username')
-#     password = request.form.get('password')
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-#     # Here, you can add your backend logic, such as authentication or database interactions
-#     # For simplicity, let's just print the received data
-#     print(f"Received login request - Username: {username}, Password: {password}")
+    # Here, you can add your backend logic, such as authentication or database interactions
+    # For simplicity, let's just print the received data
+    print(f"Received login request - Username: {username}, Password: {password}")
 
-#     return 'Login successful!'
+    return 'Login successful!'
 
 if __name__ == '__main__':
     app.run(debug=True)
